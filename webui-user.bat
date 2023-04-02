59091eafc7ab827867dd54c8d669c2a24dd27682
mehrab-haque <mehrab.haque.0001@gmail.com>
2023-04-02 17:50:15 +0600

    modified user bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--listen --api --medvram --precision full --opt-split-attention-v1 --no-half --no-half-vae --opt-sub-quad-attention --disable-nan-check interrogate gfpgan bsrgan esrgan scunet codeformer
 
 call webui.bat
